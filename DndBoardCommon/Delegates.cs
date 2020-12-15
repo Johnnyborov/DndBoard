f966c92821d91f82135a94938a4f32ec615f5d31
JohnnyBorov <JohnnyBorov@gmail.com>
2020-12-16 00:13:45 +0300

    Some half-working blazorserver project referencing DndBoardCommon with layouts+compontents

---
+++ DndBoardCommon/Delegates.cs
@@ -0,0 +1,7 @@
+﻿using System.Threading.Tasks;
+
+namespace DndBoardCommon
+{
+    public delegate Task FilesRefsChangedHandler();
+    public delegate Task BoardIdChangedHandler(string boardId);
+}
---
+++ DndBoardCommon/Store/AppState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DndBoardCommon.Helpers;
+using DndBoardCommon.Models;
+
+namespace DndBoardCommon.Store
+{
+    public class AppState
+    {
+        public ChatHubManager ChatHubManager;
+        public List<MapImage> MapImages;
+        public List<MapImage> ModelImages;
+
+        public event FilesRefsChangedHandler FilesRefsChanged;
+        public event BoardIdChangedHandler BoardIdChanged;
+
+        public async Task InvokeFilesRefsChanged()
+            => await FilesRefsChanged?.Invoke();
+        public async Task InvokeBoardIdChanged(string boardId)
+            => await BoardIdChanged?.Invoke(boardId);
+    }
+}
